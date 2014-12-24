fbaea2beff6fae7111723effc7ddf38c19343c58
Matvey-Kuk <motakuk@gmail.com>
2014-12-24 22:13:25 +0300

    Можно выгружать и загружать карту, готова анимашка

--- LeeAlgorithm/source.js
+++ LeeAlgorithm/source.js
@@ -104,7 +104,7 @@ $(document).ready(function(){
         clear_work_results();
 
         var posX = event.clientX - canvas.offsetLeft;
-        var posY = event.clientY - canvas.offsetTop;
+        var posY = event.clientY - canvas.offsetTop + $(window).scrollTop();
         var rectangle_x_size = canvasDiv.width() / quontity_of_blocks_x;
         var rectangle_y_size = canvasDiv.height() / quontity_of_blocks_y;
         var target_coords = {
@@ -174,6 +174,33 @@ $(document).ready(function(){
         edit_mode = 'border';
     });
 
+    var dump_area = $("#dump_area");
+
+    $( "#import_map" ).click(function() {
+        clear_work_results();
+        var lines = dump_area.val().split("\n");
+        for(var i = 0 ; i < lines.length ; i++){
+            var line_elements = lines[i].split("\t");
+            line_elements[0] = parseInt(line_elements[0], 10);
+            line_elements[1] = parseInt(line_elements[1], 10);
+            if(line_elements.length == 3){
+                x_y_block_types_array[line_elements[0]][line_elements[1]]['type'] = line_elements[2];
+            }
+        }
+        make_grid()
+    });
+
+    $( "#export_map" ).click(function() {
+        clear_work_results();
+        var dump_string = "";
+        for(i = 0 ; i < x_y_block_types_array.length ; i++){
+            for(j = 0 ; j < x_y_block_types_array[0].length ; j++) {
+                dump_string += i + "\t" + j + "\t" + x_y_block_types_array[i][j]['type'] + "\n";
+            }
+        }
+        dump_area.html(dump_string);
+    });
+
     var count_distances = function(){
         var shift = [-1, 0, 1];
         var a, b;
@@ -236,7 +263,6 @@ $(document).ready(function(){
                 if(x_y_block_types_array[i][j]['type'] == 'end'){
                     end_point['x'] = i;
                     end_point['y'] = j;
-                    console.log(end_point);
                 }
             }
         }
@@ -245,38 +271,55 @@ $(document).ready(function(){
         var constraint = 50;
         var i = end_point['x'];
         var j = end_point['y'];
-        while (!finish_has_been_founded) {
-            var minimal_distance = NaN;
-            var min_dist_i = NaN;
-            var min_dist_j = NaN;
+        var next_route_point_has_been_found = true;
 
-            for (var a = 0; a < shift.length; a++) {
-                for (var b = 0; b < shift.length; b++) {
-                    if ((i + shift[a] >= 0) && (j + shift[b] >= 0) && (i + shift[a] < quontity_of_blocks_x) && (j + shift[b] < quontity_of_blocks_y) && !((shift[a] == 0) && (shift[b] == 0)) && (shift[a] * shift[b] == 0)) {
-                        if (x_y_block_types_array[i + shift[a]][j + shift[b]]['type'] == 'field' && (x_y_block_types_array[i + shift[a]][j + shift[b]]['distance'] != '')) {
-                            var current_point_distance = x_y_block_types_array[i + shift[a]][j + shift[b]]['distance'];
-                            if(isNaN(minimal_distance) || minimal_distance > current_point_distance){
-                                minimal_distance = current_point_distance;
-                                min_dist_i = i + shift[a];
-                                min_dist_j = j + shift[b];
+        var find_next_route_point = function () {
+            if (!finish_has_been_founded && next_route_point_has_been_found) {
+                next_route_point_has_been_found = false;
+                var minimal_distance = NaN;
+                var min_dist_i = NaN;
+                var min_dist_j = NaN;
+
+                for (var a = 0; a < shift.length; a++) {
+                    for (var b = 0; b < shift.length; b++) {
+                        if ((i + shift[a] >= 0) && (j + shift[b] >= 0) && (i + shift[a] < quontity_of_blocks_x) && (j + shift[b] < quontity_of_blocks_y) && !((shift[a] == 0) && (shift[b] == 0)) && (shift[a] * shift[b] == 0)) {
+                            if (x_y_block_types_array[i + shift[a]][j + shift[b]]['type'] == 'field' && (x_y_block_types_array[i + shift[a]][j + shift[b]]['distance'] != '')) {
+                                var current_point_distance = x_y_block_types_array[i + shift[a]][j + shift[b]]['distance'];
+                                if(isNaN(minimal_distance) || minimal_distance > current_point_distance){
+                                    minimal_distance = current_point_distance;
+                                    min_dist_i = i + shift[a];
+                                    min_dist_j = j + shift[b];
+                                }
                             }
-                        }
-                        if(x_y_block_types_array[i + shift[a]][j + shift[b]]['type'] == 'start'){
-                            finish_has_been_founded = true;
+                            if(x_y_block_types_array[i + shift[a]][j + shift[b]]['type'] == 'start'){
+                                finish_has_been_founded = true;
+                            }
                         }
                     }
                 }
-            }
 
-            if(!finish_has_been_founded && !isNaN(minimal_distance)){
-                x_y_block_types_array[min_dist_i][min_dist_j]['type'] = 'route';
-                i = min_dist_i;
-                j = min_dist_j;
-                console.log(i,j);
-            }
-        }
+                if(!finish_has_been_founded && !isNaN(minimal_distance)){
+                    x_y_block_types_array[min_dist_i][min_dist_j]['type'] = 'route';
+                    i = min_dist_i;
+                    j = min_dist_j;
+                    next_route_point_has_been_found = true;
+                }
 
-        make_grid();
+                if(isNaN(minimal_distance)){
+                    alert('Невозможно найти путь.');
+                }
+
+                make_grid();
+
+                if(animated){
+                    setTimeout(find_next_route_point, 100);
+                } else {
+                    find_next_route_point();
+                }
+            }
+        };
+
+        find_next_route_point();
     };
 
     //Слушаю все нажатия мыши на холсте и вызываю функцию обработчик
